6dbe44cc1a3e5907fec6c9067d04e841b6e79247
Polly Yordanova <polina0829@abv.bg>
2024-09-26 18:22:06 +0300

    Add files via upload

---
+++ data_types_and_variables_exercise/chars_to_string.py
@@ -0,0 +1,6 @@
+first_character = input()
+second_character = input()
+third_character = input()
+result = (f"{first_character}{second_character}{third_character}")
+print(result)
+
---
+++ data_types_and_variables_exercise/snowballs.py
@@ -0,0 +1,18 @@
+number_of_snowballs = int(input())
+max_snowball_weight = 0
+max_snowball_time = 0
+max_snowball_value = 0
+max_snowball_quality = 0
+
+for current_snowball in range(number_of_snowballs):
+    current_weight = int(input())
+    current_time = int (input())
+    current_quality = int(input())
+    current_value = (current_weight // current_time) ** current_quality
+    if current_value > max_snowball_value:
+        max_snowball_weight = current_weight
+        max_snowball_time = current_time
+        max_snowball_value = current_value
+        max_snowball_quality = current_quality
+
+print(f"{max_snowball_weight} : {max_snowball_time} = {max_snowball_value} ({max_snowball_quality})")
